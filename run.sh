35c0296bb0da0ae4146efefdafca3ecacb68bc35
Atharva Tendle <atharva.tendle@huskers.unl.edu>
2020-10-25 22:52:00 -0500

    bugs

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@
 #SBATCH --partition=gpu
 #SBATCH --mem=0
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:2
-#SBATCH --constraint='gpu_32gb&gpu_v100'
+#SBATCH --gres=gpu:1
+#SBATCH --constraint='gpu_p100&gpu_v100'
 #SBATCH --job-name=mnist_resnet_baseline
 #SBATCH --mail-type=begin
 #SBATCH --mail-type=end
